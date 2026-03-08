cc529ba0205077f2486907b0923330b1034f67d8
Saveliy Borisov <borisov.talim@yandex.ru>
2026-03-08 16:29:39 +0300

    Tune Kokkos shared field view traits for scalar 3D functors

--- include/FDTD_kokkos/kokkos_shared.h
+++ include/FDTD_kokkos/kokkos_shared.h
@@ -6,16 +6,16 @@
 #include <vector>
 
 #include <Kokkos_Core.hpp>
-#include <Kokkos_SIMD.hpp>
 
 #include "Structures.h"
 
 
 namespace FDTD_kokkos {
-    using simd_type = Kokkos::Experimental::native_simd<double>;
-    constexpr int simd_width = int(simd_type::size());
     using Device = Kokkos::DefaultExecutionSpace;
-    using Field = Kokkos::View<double*, Kokkos::MemoryTraits<Kokkos::Aligned>>;
+    using Field = Kokkos::View<
+        FP*,
+        Kokkos::MemoryTraits<Kokkos::Restrict | Kokkos::Aligned>
+    >;
     using TimeField = std::vector<Field>;
     using Function = std::function<int(int, int, int)>;
     using InitFunction = std::function<double(double, double, double, double)>;
